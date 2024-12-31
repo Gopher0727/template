693ad6dc45a5d7ec191afebe9a5a6eb963fecdaa
Gopher <1900271083@qq.com>
2024-12-31 23:41:04 +0800

    modify

--- Category/04_Math/Comb.cpp
+++ Category/04_Math/Comb.cpp
@@ -1,3 +1,5 @@
+// 自动扩容类
+//
 struct Combinatorics {
     int n;
     std::vector<Mint> _fac, _invfac, _inv;
@@ -45,12 +47,8 @@ struct Combinatorics {
 } C;
 
 // 预处理
+//
 namespace Comb {
-    const int MX = 2e5 + 1;
-
-    ll F[MX]; // F[i] = i!
-    ll INV_F[MX]; // INV_F[i] = i!^-1
-
     ll qpow(ll a, ll b, int p) {
         ll res = 1;
         a = (a % p + p) % p;
@@ -62,12 +60,17 @@ namespace Comb {
         return res;
     }
 
+    const int MX = 2e5 + 1;
+
+    vector<ll> F, INV_F;
     auto init = [] {
+        F.resize(MX); // F[i] = i!
+        INV_F.resize(MX); // INV_F[i] = i!^-1
+
         F[0] = 1;
         for (int i = 1; i < MX; i++) {
             F[i] = F[i - 1] * i % MOD;
         }
-
         // 连续阶乘的逆元 从右往左 线性递推
         INV_F[MX - 1] = qpow(F[MX - 1], MOD - 2, MOD);
         for (int i = MX - 1; i; i--) {
@@ -100,3 +103,7 @@ Mint comb(ll n, ll m) {
     }
     return ans;
 }
+
+
+// 练习题：
+// https://leetcode.cn/problems/count-the-number-of-arrays-with-k-matching-adjacent-elements/
--- Category/04_Math/Comb.py
+++ Category/04_Math/Comb.py
@@ -1,12 +1,13 @@
 class Combinatorics:
-    def __init__(self, MX) -> None:
-        self.f = [1] * (MX + 1)
-        self.g = [1] * (MX + 1)
-        for i in range(2, MX + 1):
+    def __init__(self, N) -> None:
+        N += 1
+        self.f = [1] * N
+        self.g = [1] * N
+        for i in range(1, N):
             self.f[i] = self.f[i - 1] * i % MOD
-        self.g[-1] = pow(self.f[-1], -1, MOD)
-        for i in range(MX, 1, -1):
-            self.g[i - 1] = self.g[i] * i % MOD
+        self.g[-1] = pow(self.f[-1], MOD - 2, MOD)
+        for i in range(N - 2, -1, -1):
+            self.g[i] = self.g[i + 1] * (i + 1) % MOD
 
     def fact(self, n) -> int:
         return self.f[n]
@@ -27,20 +28,23 @@ class Combinatorics:
             return 0
         return self.f[n] * self.g[m] * self.g[n - m] % MOD
 
+    def catalan(self, n):
+        return (self.comb(2 * n, n) - self.comb(2 * n, n - 1)) % MOD
+
 
 C = Combinatorics(2 * 10**5)
 
 
 # 预处理
 MOD = 1_000_000_007
-MX = 41
+N = 41
 
-fac = [0] * MX  # f[i] = i!
+fac = [0] * N  # f[i] = i!
 fac[0] = 1
-for i in range(1, MX):
+for i in range(1, N):
     fac[i] = fac[i - 1] * i % MOD
 
-inv_f = [0] * MX  # inv_f[i] = i!^-1
+inv_f = [0] * N  # inv_f[i] = i!^-1
 inv_f[-1] = pow(fac[-1], -1, MOD)
-for i in range(MX - 1, 0, -1):
+for i in range(N - 1, 0, -1):
     inv_f[i - 1] = inv_f[i] * i % MOD
