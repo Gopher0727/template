46cfdc13d3236a0ef970de071b67449067c2dfba
Gopher <1900271083@qq.com>
2025-01-02 22:24:24 +0800

    modify

--- Category/07_Others/sort.cpp
+++ Category/07_Others/sort.cpp
@@ -1,23 +1,6 @@
-// 选择排序：每次找出第 i 小的元素，与数组第 i 个位置上的元素交换
-void selection_sort(int* a, int n) {
-    if (n < 2) {
-        return;
-    }
-    for (int i = 0; i < n - 1; ++i) {
-        int idx = i;
-        for (int j = i + 1; j < n; ++j) {
-            if (a[j] < a[idx]) {
-                idx = j;
-            }
-        }
-        if (idx != i) {
-            swap(a[i], a[idx]);
-        }
-    }
-}
-
 // 冒泡排序：每次检查相邻两个元素（可以用 flag 标记是否交换过元素）
-void bubble_sort(int* a, int n) {
+void bubble_sort(vector<int>& a) {
+    int n = a.size();
     if (n < 2) {
         return;
     }
@@ -33,8 +16,30 @@ void bubble_sort(int* a, int n) {
     }
 }
 
+
+// 选择排序：每次找出第 i 小的元素，与数组第 i 个位置上的元素交换
+void selection_sort(vector<int>& a) {
+    int n = a.size();
+    if (n < 2) {
+        return;
+    }
+    for (int i = 0; i < n - 1; ++i) {
+        int idx = i;
+        for (int j = i + 1; j < n; ++j) {
+            if (a[j] < a[idx]) { // 稳定
+                idx = j;
+            }
+        }
+        if (idx != i) {
+            swap(a[i], a[idx]);
+        }
+    }
+}
+
+
 // 插入排序：从后往前找，比当前值大的右移一位（将数组分成了有序和无序两部分）
-void insertion_sort(int* a, int n) {
+void insertion_sort(vector<int>& a) {
+    int n = a.size();
     if (n < 2) {
         return;
     }
@@ -49,38 +54,53 @@ void insertion_sort(int* a, int n) {
     }
 }
 
-void insertion_sort() {
-    std::cout << "\n" "用 `rotate` 实现插入排序，逐步运行:\n";
 
-    s = {'2', '4', '2', '0', '5', '9', '7', '3', '7', '1'};
-
-    for (auto i = s.begin(); i != s.end(); ++i) {
-        std::cout << "i = " << std::ranges::distance(s.begin(), i) << ": ";
-        std::ranges::rotate(std::ranges::upper_bound(s.begin(), i, *i), i, i + 1);
-        std::cout << s << '\n';
-    }
-    std::cout << (std::ranges::is_sorted(s) ? "已排序!" : "未排序.") << '\n';
-}
-
-// 折半插入排序
-void half_insertion_sort(int* a, int n) {
-    if (n < 2) {
-        return;
-    }
-    for (int i = 1; i < n; ++i) {
-        int v = a[i];
-        auto idx = upper_bound(a, a + i, v) - a;
-        memmove(a + idx + 1, a + idx, (i - idx) * sizeof(int));
-        a[idx] = v;
+// 折半插入排序  `rotate`
+void half_insertion_sort(vector<int>& a) {
+    for (auto i = a.begin(); i != a.end(); ++i) {
+        rotate(upper_bound(a.begin(), i, *i), i, i + 1);
     }
 }
+
+
+// 快速排序：找基准，分治
+int partition(vector<int>& a, int low, int high) {
+    int pivot = a[high]; // 选择最后一个元素作为枢轴
+    int i = low - 1; // 较小元素的索引
+    for (int j = low; j < high; ++j) {
+        if (a[j] <= pivot) {
+            swap(a[++i], a[j]);
+        }
+    }
+    swap(a[++i], a[high]);
+    return i;
+}
+void quick_sort(vector<int>& a, int low = 0, int high = -1) {
+    if (high == -1) {
+        high = (int) a.size() - 1;
+    }
+    if (low < high) {
+        int pi = partition(a, low, high);
+        quick_sort(a, low, pi - 1);
+        quick_sort(a, pi + 1, high);
+    }
+}
+
+
+// 桶排序
+// todo
+
+
+// 希尔排序
+// todo
+
 
 // 计数排序：需要一个额外空间存储待排序数组中相应元素的个数，还需要计算前缀和解决元素重复的情况
+// todo
 const int N = 10'010;
 const int W = 10'010;
 vector<int> a(N), b(N), cnt(W);
-int n, w;
-void counting_sort() {
+void counting_sort(int n, int w) {
     for (int i = 0; i < n; ++i) {
         cnt[a[i]]++;
     }
@@ -89,5 +109,82 @@ void counting_sort() {
     }
     for (int i = n - 1; i >= 0; --i) {
         b[cnt[a[i]]--] = a[i];
+    }
+}
+
+
+// 基数排序
+// 计数排序辅助函数，用于基数排序
+void counting_sort_for_radix(vector<int>& arr, int exp) {
+    int n = arr.size();
+
+    const int N = 10;
+    vector<int> cnt(N);
+    for (int i = 0; i < n; i++) {
+        cnt[(arr[i] / exp) % 10]++;
+    }
+    for (int i = 1; i < N; i++) {
+        cnt[i] += cnt[i - 1];
+    }
+
+    vector<int> output(n);
+    for (int i = n - 1; i >= 0; i--) {
+        output[cnt[(arr[i] / exp) % 10] - 1] = arr[i];
+        cnt[(arr[i] / exp) % 10]--;
+    }
+
+    arr = std::move(output);
+}
+void radix_sort(vector<int>& arr) {
+    int max = ranges::max(arr);
+    for (int exp = 1; max / exp > 0; exp *= 10) {
+        counting_sort_for_radix(arr, exp);
+    }
+}
+
+
+// 堆排序
+void heapify(std::vector<int>& a, int n, int i) {
+    int idx = i;
+    if (int L = 2 * i + 1; L < n && a[L] > a[idx]) {
+        idx = L;
+    }
+    if (int R = 2 * i + 2; R < n && a[R] > a[idx]) {
+        idx = R;
+    }
+    if (idx != i) {
+        swap(a[i], a[idx]);
+        heapify(a, n, idx);
+    }
+}
+void heapSort(std::vector<int>& a) {
+    int n = a.size();
+    for (int i = n / 2 - 1; i >= 0; --i) {
+        heapify(a, n, i);
+    }
+    for (int i = n - 1; i > 0; --i) {
+        swap(a[0], a[i]);
+        heapify(a, i, 0);
+    }
+}
+
+
+// 测试验证
+void solve() {
+    mt19937_64 gen(chrono::duration_cast<chrono::milliseconds>(chrono::system_clock::now().time_since_epoch()).count());
+    uniform_int_distribution<int> dis(1, 100);
+
+    for (int i = 0; i < 10; ++i) {
+        vector<int> a(10);
+        for (int i = 0; i < a.size(); ++i) {
+            auto rnd = dis(gen);
+            a[i] = rnd;
+        }
+        debug(a);
+        radix_sort(a);
+        debug(a);
+        if (is_sorted(a.begin(), a.end())) {
+            cout << "True\n";
+        }
     }
 }
