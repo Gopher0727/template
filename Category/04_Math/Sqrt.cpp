83e971869b1d0eb747866f2ef4d25646fd2ad89d
Gopher <1900271083@qq.com>
2025-01-11 18:00:53 +0800

    modify

--- Category/04_Math/Sqrt.cpp
+++ Category/04_Math/Sqrt.cpp
@@ -1,5 +1,5 @@
 namespace Sqrt {
-    // x * x <= n
+    // x * x <= n，返回最大的 x
     ll mSqrt(ll n) {
         ll x = sqrt(n);
         while (x * x > n) {
@@ -10,14 +10,22 @@ namespace Sqrt {
         }
         return x;
     }
-    // x * (x+1) / 2 <= x
-    ll mGet(ll n) {
+    // x * (x+1) / 2 >= n，返回最小的 x
+    ll mget(ll n) {
         ll x = sqrt(2.0L * n);
         while (x * (x + 1) / 2 < n) {
             x++;
         }
         while (x * (x - 1) / 2 + 1 > n) {
             x--;
+        }
+        return x;
+    }
+    // x * (x+1) / 2 > n，返回最小的 x
+    ll mGet(ll n) {
+        ll x = std::sqrt(2.0L * n);
+        while (x * (x + 1) <= 2 * n) {
+            x++;
         }
         return x;
     }
