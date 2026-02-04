46e92b0b8690b103c1ea94171b27dcbba49246dd
Gopher <gopher0727@gmail.com>
2026-02-04 19:17:56 +0800

    add some methods

--- Go/Array.go
+++ Go/Array.go
@@ -1,4 +1,13 @@
-// Reverse [l, r] 闭区间
+// RangeSum [l, r] 闭区间求和
+func RangeSum[T any](a []T, l, r int) (ans T) {
+	for l <= r {
+		ans += a[l]
+		l++
+	}
+	return
+}
+
+// Reverse [l, r] 闭区间翻转
 func Reverse[T any](a []T, l, r int) {
 	for l < r {
 		a[l], a[r] = a[r], a[l]
@@ -7,6 +16,7 @@ func Reverse[T any](a []T, l, r int) {
 	}
 }
 
+// PrintArray 在一行打印数组元素，并按空格分开
 func PrintArray[T any](a []T) {
 	for _, x := range a {
 		fmt.Fprint(out, x, " ")
