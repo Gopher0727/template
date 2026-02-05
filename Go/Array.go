2ebbbec537a798df147a6b5683168bd84ac7e6db
Gopher <gopher0727@gmail.com>
2026-02-05 20:37:41 +0800

    add some methods

--- Go/Array.go
+++ Go/Array.go
@@ -1,12 +1,22 @@
-type Number interface {
-	~int | ~int8 | ~int16 | ~int32 | ~int64 |
+// constraints
+type (
+	Signed interface {
+		~int | ~int8 | ~int16 | ~int32 | ~int64
+	}
+	Unsigned interface {
 		~uint | ~uint8 | ~uint16 | ~uint32 | ~uint64 |
-		~float32 | ~float64 |
-		~complex64 | ~complex128
-}
+			~uintptr
+	}
+	Integer interface{ Signed | Unsigned }
+	Float   interface{ ~float32 | ~float64 }
+	Real    interface{ Integer | Float }
+	Ordered interface{ Real | ~string }
+	Complex interface{ ~complex64 | ~complex128 }
+	Number  interface{ Real | Complex }
+)
 
 // RangeSum [l, r] 闭区间求和
-func RangeSum[T Number](a []T, l, r int) (ans T) {
+func RangeSum[T Number | string](a []T, l, r int) (ans T) {
 	for l <= r {
 		ans += a[l]
 		l++
