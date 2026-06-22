8d766a3e64339fd1c7d0ae5765bed22b2ea57519
Gopher <gopher0727@gmail.com>
2026-06-22 18:10:03 +0800

    upd

--- Python/DP.py
+++ Python/DP.py
@@ -4,32 +4,23 @@ def DigitalDP(low: int, high: int) -> int:
     high_s = list(map(int, str(high)))
     n = len(high_s)
     low_s = list(map(int, str(low).zfill(n)))
-    # diff_lh = n - len(low_s)
 
     @cache
-    def dfs(i: int, diff: int, limit_low: bool, limit_high: bool) -> int:
+    def dfs(i: int, diff: int, limit_low: bool, limit_high: bool) -> int: # todo
         if i == n:
             return 1 if diff == 0 else 0
 
-        # lo = low_s[i - diff_lh] if limit_low and i >= diff_lh else 0
         lo = low_s[i] if limit_low else 0
         hi = high_s[i] if limit_high else 9
 
         res = 0
-        # start = lo
-        # if limit_low and i < diff_lh:
-        #     res = dfs(i + 1, diff, True, False)
-        #     start = 1
-
-        # for d in range(start, hi + 1):
         for d in range(lo, hi + 1):
             res += dfs(
                 i + 1,
-                diff + (d if i % 2 == 1 else -d),
+                diff + (d if i % 2 == 1 else -d), # todo
                 limit_low and d == lo,
                 limit_high and d == hi,
             )
-
         return res
 
     return dfs(0, 0, True, True)
